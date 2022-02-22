82c191cab11d1e6c4f9cd70a90bed90428dcd231
Isaac Oduh <isaac.oduh@habaripay.com>
2022-02-22 12:59:20 +0100

    create seeders for admin user and regular users

---
+++ database/seeders/AdminTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class AdminTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $now = now();
+        $adminList = [
+            [
+                'id' => 1,
+                'first_name' => 'admin',
+                'last_name' => 'domain',
+                'email' => 'admin@domain.com',
+                'password' => bcrypt('password'),
+                'uuid' => Str::uuid(),
+                'is_admin' => 1,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ],
+        ];
+        foreach($adminList as $admin) {
+            DB::table('users')->updateOrInsert(['id' => $admin['id']], $admin);
+        }
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        Model::unguard(); // tempoarary disble mass exception
+        DB::beginTransaction();
+
+        $this->call(AdminTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+
+        DB::commit();
+        Model::reguard();
+
     }
 }
